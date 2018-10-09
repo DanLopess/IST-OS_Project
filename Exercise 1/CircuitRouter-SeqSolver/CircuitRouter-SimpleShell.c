2a375729afc1baa2a8ab4ca28040012035937237
Nuno Ramos <nuno.m.ramos@tecnico.ulisboa.pt>
2018-10-09 17:47:30 +0100

    Shell
    Completei um pouco o shell, falta umas coisas q ainda n pensei em como fazer como a parte de correr o seqsolver mas ja la esta ua frameworkzita

--- Exercise 1/CircuitRouter-SeqSolver/CircuitRouter-SimpleShell.c
+++ Exercise 1/CircuitRouter-SeqSolver/CircuitRouter-SimpleShell.c
@@ -3,20 +3,54 @@ CircuitRouter-SimpleShell
 
 Developed by Daniel Lopes & Nuno Ramos, IST
  =============================================================================*/
+#include <string.h>
+
 
 int main(int argc, char** argv){
 	/*
 	 * Initialization
 	 */
 
-	/* Main functionality */
-	if(argc == 0 || argc == 1){ /*No more than 1 argument allowed*/
-		if(argc)
+	 int maxChildren, pid;
+	 int currentChildren = 0;
+	 int exit = 0;
+	 char command[201]; /*large size to acomodate large file names*/
 
+	/* Main functionality */
+	if (argc == 0 || argc == 1) { /*No more than 1 argument allowed*/
+		if (argc == 0)
+			maxChildren = -1; /*unlimited children*/
+		else
+			maxChildren = argv[0];
 	}
 	else{
 		printf("Error! Too many arguments.");
 		exit(1);
 	}
+
+	while (!exit) {
+		scanf("%s", command);
+
+		if (!strcmp(command, "exit")) {
+			exit = 1;
+		}
+
+		else if (!strcmp(command, "run")) {
+			scanf("%s", command);
+
+			if (maxChildren == -1 || currentChildren < maxChildren) {
+				pid = fork();
+				if (pid != 0) {
+					/*TODO part that runs code*/
+				}
+			}
+
+			else { /*too many children to create another */
+				/*wait() TODO nao faco ideia como se usa o wait mas acho q precisa*/
+			}
+		}
+	}
+
+
 	return 0;
 }

5189b1043ad05d58df3097c87da7d29e49d8a05a
Daniel <daniel.ramos.lopes@tecnico.ulisboa.pt>
2018-10-10 14:01:40 +0100

    changes to shell

--- Exercise 1/CircuitRouter-SeqSolver/CircuitRouter-SimpleShell.c
+++ Exercise 1/CircuitRouter-SeqSolver/CircuitRouter-SimpleShell.c
@@ -5,35 +5,22 @@ Developed by Daniel Lopes & Nuno Ramos, IST
  =============================================================================*/
 #include <string.h>
 
+void runSeqSolver(){
+	// TODO this function when called, runs seqsolver with a given
+	// input file
+}
 
-int main(int argc, char** argv){
-	/*
-	 * Initialization
-	 */
-	 int maxChildren, pid;
-	 int currentChildren = 0;
-	 int exit = 0;
-	 char command[201]; /*large size to acomodate large file names*/
+void parseCommand(int maxChildren){
+	int pid;
+	int currentChildren = 0;
+	char command[256]; /*large size to acomodate large file names*/
 
-	/* Main functionality */
-	if (argc == 0 || argc == 1) { /*No more than 1 argument allowed*/
-		if (argc == 0)
-			maxChildren = -1; /*unlimited children*/
-		else
-			maxChildren = argv[0];
-	}
-	else{
-		printf("Error! Too many arguments.");
-		exit(1);
-	}
-
-	while (!exit) {
+	while() {
 		scanf("%s", command);
 
 		if (!strcmp(command, "exit")) {
-			exit = 1;
+			exit(1);
 		}
-
 		else if (!strcmp(command, "run")) {
 			scanf("%s", command);
 
@@ -45,13 +32,39 @@ int main(int argc, char** argv){
 					/*TODO part that runs code*/
 				}
 			}
-
 			else { /*too many children to create another */
 				/*wait() TODO nao faco ideia como se usa o wait mas acho q precisa*/
 			}
 		}
+		else{
+			printf("Invalid command!\n");
+		}
 	}
 
+}
 
+
+/* =============================================================================
+ * main
+ * =============================================================================
+ */
+int main(int argc, char** argv){
+	/*
+	 * Initialization
+	 */
+
+	/* Main functionality */
+	if (argc == 0 || argc == 1) { /*No more than 1 argument allowed*/
+		int maxChildren;
+		if (argc == 0)
+			maxChildren = -1; /* unlimited children */
+		else
+			maxChildren = argv[0];
+		parseCommand(maxChildren);
+	}
+	else{
+		printf("Error! Too many arguments.");
+		exit(1);
+	}
 	return 0;
 }

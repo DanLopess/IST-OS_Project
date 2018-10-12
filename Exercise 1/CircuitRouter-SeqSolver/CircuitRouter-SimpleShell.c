658ba618b0463ef8a769c79f6c07b5bd5dbb231b
nmiguel <nuno.m.ramos@tecnico.ulisboa.pt>
2018-10-12 18:35:07 +0100

    added child success
    also i now shows index of finished children

--- Exercise 1/CircuitRouter-SeqSolver/CircuitRouter-SimpleShell.c
+++ Exercise 1/CircuitRouter-SeqSolver/CircuitRouter-SimpleShell.c
@@ -25,33 +25,40 @@ void create_child(int* childrenPIDs, int* index, int *currentChildren, char **ar
 }
 
 void parseCommand(int maxChildren){
-	int *childrenPIDs ;
+	int *childrenPIDs, *childrenSuccess;
 	int index = 0; /* Index of last childrenPID inserted into array */
 	int maxIndex = maxChildren;
 	int currentChildren = 0; /* Control the number of children executing */
-	int status, i;
+	int status, i = 0;
 	char *argVector[3], line[256];
 	int argVectorSize = 3, bufferSize = 256;
 
 /*Initialization*/
 	if(maxChildren >= 0){
 		childrenPIDs = (int*) malloc(sizeof(int)*maxChildren);
+		childrenSuccess = (int*) malloc(sizeof(int)*maxChildren);
 	} else{
 		childrenPIDs = (int*) malloc(sizeof(int)*10);
+		childrenSuccess = (int*) malloc(sizeof(int)*10);
 	}
 
 	while (1) {
 		int result = readLineArguments(argVector, argVectorSize, line, bufferSize);
 		if (result == 1 && !strcmp(argVector[0], "exit")) {
-			for (i = 0; i < index; i++){
-				if (waitpid(childrenPIDs[i], &status, WIFEXITED(status))){
+			for (; i < index; i++) /*Waits for all unfinished children*/
+				childrenSuccess[i] = waitpid(childrenPIDs[i], &status, WIFEXITED(status));
+
+			for (i = 0; i < index; i++) {
+				if (childrenSuccess[i]) {
 					printf("CHILD EXITED (PID=%d; return OK)\n", childrenPIDs[i]);
 				} else{
 					printf("CHILD EXITED (PID=%d; return NOK)\n", childrenPIDs[i]);
 				}
 			}
+
 			printf("END.\n");
 			free(childrenPIDs);
+			free(childrenSuccess);
 			exit(0);
 		}
 
@@ -59,14 +66,16 @@ void parseCommand(int maxChildren){
 			if (maxChildren == -1 || currentChildren < maxChildren) {
 				if (index == maxIndex) {
 					childrenPIDs = realloc(childrenPIDs, (sizeof(int)*maxIndex + 10));/* Increases size of array*/
+					childrenSuccess = realloc(childrenPIDs, (sizeof(int)*maxIndex + 10));
 					maxIndex = maxIndex + 10;
 				}
 				create_child(childrenPIDs, &index, &currentChildren, argVector);
 			}
 
 			else { /*too many children to create another */
-				waitpid(-1, &status, 0); /* Wait for any child to terminate (at least one)*/
+				childrenSuccess[i] = waitpid(-1, &status, 0); /* Wait for any child to terminate (at least one)*/
 				currentChildren--;
+				i++;
 				create_child(childrenPIDs, &index, &currentChildren, argVector);
 			}
 		}

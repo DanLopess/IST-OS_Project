5447798fb7d3138174845eab0000eb879173ae55
Daniel <36997776+DanLopess@users.noreply.github.com>
2018-10-22 23:33:43 +0100

    asd

---
+++ Exercise 2/Exercise 2 - In development/threadsTest.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <getopt.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <pthread.h>
+
+int main(int argc, char const *argv[]) {
+
+
+
+	return 0;
+}

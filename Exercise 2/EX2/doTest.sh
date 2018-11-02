0e3d29c05fec66537c3a2bc062c143d5a2038ec7
DanLopess <daniel.ramos.lopes@tecnico.ulisboa.pt>
2018-11-02 16:00:01 +0000

    pushed

--- Exercise 2/EX2/doTest.sh
+++ Exercise 2/EX2/doTest.sh
@@ -8,27 +8,15 @@ echo "Speed_up: Efficiency meter"
 option=$1
 inputName=$2
 
-echo $inputName
-
-cd CircuitRouter-SeqSolver
-
-for entry in "$search_dir"/*
-do
-  echo "$entry"
-done
-
-seqTime=time ./CircuitRouter-SeqSolver $inputName # execute sequential
+seqTime=time ./CircuitRouter-SeqSolver/CircuitRouter-SeqSolver $inputName # execute sequential
 echo $seqTime
 echo "#threads, exec_time, speedup"
 echo "1S,$seqTime,1"
 
-cd ..
-cd CircuitRouter-ParSolver
-
 var=1
 while [ $var -le $option ]
 do # Runs ParSolver with 1 to n threads, n times
-  parTime=time ./CircuitRouter-ParSolver -t $var $inputName
+  parTime=time ./CircuitRouter-ParSolver/CircuitRouter-ParSolver -t $var $inputName
   speedup=$(echo "scale=6; ${seqTime}/${parTime}" | bc)
   echo "$var,$parTime,$speedup"
 	var=$((var+1))

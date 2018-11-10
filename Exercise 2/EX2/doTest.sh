6948219eea96153b26088dda114179d92b3220eb
Daniel <36997776+DanLopess@users.noreply.github.com>
2018-11-10 13:24:45 +0000

    created global makefile

--- Exercise 2/EX2/doTest.sh
+++ Exercise 2/EX2/doTest.sh
@@ -5,15 +5,15 @@ echo "====== Speed_up: Efficiency meter ======"
 option=$1
 inputName=$2
 seqTime="time (./CircuitRouter-SeqSolver/CircuitRouter-SeqSolver $inputName) -R 2>&1 | grep \^user"# execute sequential
-echo $seqTime
-echo "#threads, exec_time, speedup"
-echo "1S, $seqTime, 1"
+echo $seqTime >
+echo "#threads, exec_time, speedup" > "$inputName.res"
+echo "1S, $seqTime, 1" > "$inputName.res"
 var=1
 while [ $var -le $option ]
 do # Runs ParSolver with 1 to n threads, n times
   parTime="time (./CircuitRouter-ParSolver/CircuitRouter-ParSolver -t $var $inputName) -R 2>&1 | grep \^user"
   speedup=$(echo "scale=6; ${seqTime}/${parTime}" | bc)
-  echo "$var,$parTime,$speedup"
+  echo "$var,$parTime,$speedup" > "$inputName.res"
 	var=$((var+1))
 done
 echo "====== Leaving script... ======"

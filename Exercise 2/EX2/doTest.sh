1a9d1663b20d86e556ac3de4d01c9676bd7ea2fe
nmiguel <nuno.m.ramos@tecnico.ulisboa.pt>
2018-11-12 11:52:48 +0000

    dotest.sh

--- Exercise 2/EX2/doTest.sh
+++ Exercise 2/EX2/doTest.sh
@@ -4,19 +4,16 @@
 echo "====== Speed_up: Efficiency meter ======"
 option=$1
 inputName=$2
-echo "$inputName"
-execute="."`pwd`"/$output"
-seqTime="time"
-seqTime=time ./CircuitRouter-SeqSolver/CircuitRouter-SeqSolver $inputName -R 2>&1 | grep \^user
-echo "test" > "$inputName.res"
-echo "threads, exec_time, speedu" > "$inputName.res"
-echo "1S, "$seqTime", 1" > "$inputName.res"
-var=1
-while [ $var -le $option ]
+./CircuitRouter-SeqSolver/CircuitRouter-SeqSolver $inputName
+seqtime=`grep "seconds" $inputName.res | cut -d = -f2 |cut -d s -f1`
+echo "threads, exec_time, speedup" > $inputName.speedup.csv
+echo "1S, "$seqtime", 1" >> $inputName.speedup.csv
+for i in $(seq 1 $option)
 do
-  parTime="time (./CircuitRouter-ParSolver/CircuitRouter-ParSolver -t $var $inputName) -R 2>&1 | grep \^user"
-  speedup=$(echo "scale=6; ${seqTime}/${parTime}" | bc)
-  echo "$var,$parTime,$speedup" > "$inputName.res"
+	./CircuitRouter-ParSolver/CircuitRouter-ParSolver -t $i $inputName
+	partime=`grep "seconds" $inputName.res | cut -d = -f2 |cut -d s -f1`
+	speedup=$(echo "scale=6; ${seqTime}/${parTime}" | bc)
+	echo "$var,$parTime,$speedup" >> $inputName.speedup.csv
 	var=$((var+1))
 done
 echo "====== Leaving script... ======"
